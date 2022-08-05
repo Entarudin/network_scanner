39c16552401cbf1c0643b21cd8634f97eba0cbce
Entarudin <riazanov@sfedu.ru>
2022-08-05 21:21:07 +0300

    refactoring structure

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 # ip_tools
 
-## Чтобы запустить проект, нужно установить:
+## Чтобы запустить проект, нужно в терминале выполнить команды:
 
-- arp-scan: sudo apt install arp-scan
-- nmap: sudo apt-get install nmap
-- hping3: sudo apt install hping3 (для атак на порты)
-- sudo apt install python3.10-venv (виртуальное окружение) 
-- sudo apt install python3-pip (pip)
-- pip install -r requirements.txt (установка зависимостей)
-- 
-
-## Запуск проекта из под рута:
-- sudo python3 main.py (если через системынй python)
-- sudo python3 attacs/attack_on_ports.py (атака на порты)
+- chmod ugo+x start_main
+- sudo ./start_main
--- main.py
+++ main.py
@@ -1,49 +1,25 @@
-import subprocess
 import json
 import xmltodict
-from loggers import app_logger
-# REPORT FILES
-REPORT_SYSTEM_INFORMATION_JSON = 'info_system.json'
-REPORT_SCAN_TCP_PORT_XML = 'scan_tcp_port.xml'
-REPORT_SCAN_UDP_PORT_XML = 'scan_udp_port.xml'
-REPORT_SCAN_PROTOCOLS_XML = 'scan_protocols.xml'
-
-# COMMAND
-GET_NETWORK_INTERFACES_COMMAND = "ip addr | grep inet|grep -v 'inet6'|awk '{print $NF, $2}'"
-GET_IP_COMMAND = 'hostname -I | awk \'{print $1}\''
-GET_HOSTS_BY_INTERFACE_COMMAND = "arp-scan --interface={} --localnet"
-GET_HOSTNAME_COMMAND = 'cat /etc/hostname'
-GET_DISTRIBUTION_VERSION_COMMAND = 'lsb_release -a'
-GET_INFO_CORE_COMMAND = 'uname -a'
-GET_SERVICES_STATUS_COMMAND = 'systemctl show servicename --no-page'
-GET_INSTALL_PACKAGES_COMMAND = r"dpkg-query -f '${Package} ${Version} \n' -W"
-GET_SYSTEM_INFORMATION_COMMAND = f"lshw -json > {REPORT_SYSTEM_INFORMATION_JSON}"
-
-# NMAP
-SCAN_TCP_PORT_COMMAND = "nmap -p 0-49151 -sT -oX {} {}"
-SCAN_UPD_PORT_COMMAND = "nmap  -sU -oX {} {}"
-SCAN_PROTOCOLS_COMMAND = "sudo nmap -sO -oX {} {}"
-
-
-class ShellCommandsExecutor:
-    def __init__(self, command: str):
-        self.command = command
-
-    def execute(self, *args) -> str:
-        command = self.__format_command(self.command, args)
-        app_logger.info(f"execute command: {command}")
-        result = subprocess.run(
-            command,
-            shell=True,
-            stdout=subprocess.PIPE,
-            encoding='utf-8',
-        )
-        return result.stdout.strip()
-
-    def __format_command(self, command: str, command_args: tuple) -> str:
-        if not command_args:
-            return command
-        return command.format(*command_args)
+from reports_files import (
+    REPORT_SYSTEM_INFORMATION_JSON,
+    REPORT_SCAN_TCP_PORT_XML,
+    REPORT_SCAN_UDP_PORT_XML,
+    REPORT_SCAN_PROTOCOLS_XML,
+)
+from structure import (
+    get_ip_shell_executor,
+    get_network_interfaces_executor,
+    get_hosts_by_network_interface_executor,
+    get_hostname_executor,
+    get_distribution_version_executor,
+    get_services_status_executor,
+    get_installed_packages_executor,
+    get_json_file_with_system_information_executor,
+    get_info_core_executor,
+    get_scan_xml_report_to_tcp_ports_executor,
+    get_scan_xml_report_to_udp_ports_executor,
+    get_scan_xml_report_to_protocols_executor
+)
 
 
 def parse_hosts(command_output: str) -> list:
@@ -67,11 +43,11 @@ def parse_distribution_version_to_dict(command_output: str) -> dict:
     dict_info_distribution_version = {}
     list_distribution_version_with_tabs = command_output.split("\n")
     for item in list_distribution_version_with_tabs:
-        chunk = item.split('\t')
-        key = chunk[0].strip(':').lower()
+        chunks = item.split('\t')
+        key = chunks[0].strip(':').lower()
         if 'distributor' in key:
             key = '_'.join(key.split(" "))
-        value = chunk[1]
+        value = chunks[1]
         dict_info_distribution_version[key] = value
     return dict_info_distribution_version
 
@@ -80,12 +56,12 @@ def parse_services_status_to_list(command_output: str) -> list:
     result = []
     list_services_status = command_output.split('\n')
     for item in list_services_status:
-        chunk = item.split('=')
-        key = chunk[0]
-        value = chunk[1]
+        chunks = item.split('=')
+        name = chunks[0]
+        status = chunks[1]
         result.append({
-            "name": key,
-            "status": value
+            "name": name,
+            "status": status
         })
     return result
 
@@ -94,9 +70,9 @@ def parse_installed_packages_to_list(command_output: str) -> list:
     result = []
     list_installed_packages = command_output.split('\n')
     for item in list_installed_packages:
-        chunk = item.split(' ')
-        package = chunk[0]
-        version = chunk[1]
+        chunks = item.split(' ')
+        package = chunks[0]
+        version = chunks[1]
         result.append({
             "name": package,
             "version": version
@@ -164,19 +140,6 @@ def parse_xml_to_dict(filename: str) -> dict:
 
 
 def main():
-    get_ip_shell_executor = ShellCommandsExecutor(GET_IP_COMMAND)
-    get_network_interfaces_executor = ShellCommandsExecutor(GET_NETWORK_INTERFACES_COMMAND)
-    get_hosts_by_network_interface_executor = ShellCommandsExecutor(GET_HOSTS_BY_INTERFACE_COMMAND)
-    get_hostname_executor = ShellCommandsExecutor(GET_HOSTNAME_COMMAND)
-    get_distribution_version_executor = ShellCommandsExecutor(GET_DISTRIBUTION_VERSION_COMMAND)
-    get_info_core_executor = ShellCommandsExecutor(GET_INFO_CORE_COMMAND)
-    get_services_status_executor = ShellCommandsExecutor(GET_SERVICES_STATUS_COMMAND)
-    get_installed_packages_executor = ShellCommandsExecutor(GET_INSTALL_PACKAGES_COMMAND)
-    get_json_file_with_system_information_executor = ShellCommandsExecutor(GET_SYSTEM_INFORMATION_COMMAND)
-    get_scan_xml_report_to_tcp_ports_executor = ShellCommandsExecutor(SCAN_TCP_PORT_COMMAND)
-    get_scan_xml_report_to_udp_ports_executor = ShellCommandsExecutor(SCAN_UPD_PORT_COMMAND)
-    get_scan_xml_report_to_protocols_executor = ShellCommandsExecutor(SCAN_PROTOCOLS_COMMAND)
-
     ip = get_ip_shell_executor.execute()
     list_network_interface = get_network_interfaces_executor.execute().split('\n')
     network_interface = get_network_interface_by_ip(list_network_interface, ip)
