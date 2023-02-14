fbf2021f2ea6af3ba1c7ad1e7a40026eff6c512f
Entarudin <riazanov@sfedu.ru>
2023-02-14 14:44:27 +0300

    refacroring

---
+++ field_report_file.py
@@ -0,0 +1,16 @@
+field_report_requirements = {
+    "ip_address": True,
+    "hostname": True,
+    "networks_interfaces": True,
+    "current_network_interface": True,
+    "distribution_version": True,
+    "core": True,
+    "arp_table": True,
+    "services_status": True,
+    "installed_packages": True,
+    "system_information": True,
+    "tcp_ports": True,
+    "udp_ports": False,
+    "ip_ports": False,
+    "applications": True
+}
